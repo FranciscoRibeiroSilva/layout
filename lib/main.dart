51b07a691c302549bdc5b9125bdda23a8de36309
FranciscoRibeiroSilva <franciscoribeirsil@gmail.com>
2023-06-09 17:48:51 -0300

    icones e parte do botão expansivel

--- lib/main.dart
+++ lib/main.dart
@@ -60,9 +60,24 @@ class MyCard extends StatelessWidget {
         color: corCard,
       ),
       child: const Row(children: [
-        Text('Novos pedidos'),
-        Text('Novos clientes'),
-        Text('Novas cidades'),
+        Column(
+          children: [
+            Icon(Icons.shop_two),
+            Text('Novos pedidos'),
+          ],
+        ),
+        Column(
+          children: [
+            Icon(Icons.people),
+            Text('Novos clientes'),
+          ],
+        ),
+        Column(
+          children: [
+            Icon(Icons.apartment),
+            Text('Novas cidades'),
+          ],
+        ),
       ]),
     );
   }
@@ -71,16 +86,81 @@ class MyCard extends StatelessWidget {
 class CardRend extends StatelessWidget {
   const CardRend({super.key});
   final Color corCard = const Color.fromARGB(255, 215, 211, 226);
+  final Color corText = const Color.fromARGB(255, 33, 4, 87);
   @override
   Widget build(BuildContext context) {
     return Container(
         decoration: BoxDecoration(color: corCard),
-        child: const Row(
+        child: Row(
           children: [
-            Column(
+            Icon(
+              Icons.shop_2,
+              color: corText,
+            ),
+            const Column(
               children: [Text('RS 34.000,00'), Text('em novos pedidos')],
             )
           ],
         ));
+  }
+}
+
+class ExpandableFab extends StatefulWidget {
+  const ExpandableFab({
+    super.key,
+    this.initialOpen,
+    required this.distance,
+    required this.children,
+  });
+  final bool? initialOpen;
+  final double distance;
+  final List<Widget> children;
+
+  @override
+  State<ExpandableFab> createState() => _ExpandableFabState();
+}
+
+class _ExpandableFabState extends State<ExpandableFab> {
+  bool _open = false;
+
+  @override
+  void initState(){
+    super.initState();
+    _open = widget.initialOpen ?? false;
+  }
+
+  void _toggle(){
+    setState((){
+      _open = !_open;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.expand(
+      child: Stack(
+        alignment: Alignment.bottomRight,
+        clipBehavior: Clip.none,
+        children: [
+          _buildTapToCloseFab();
+          _buildTapToOpenFab();
+        ],
+      ),
+    );
+  }
+  _buildTapToCloseFab(){
+    return SizedBox(
+      width: 56.0,
+      height: 56.0,
+      child: Material(
+        shape: const CircleBorder(),
+        clipBehavior: Clip.antiAlias,
+        elevation: 4.0,
+        child: InkWell(
+          onTap: _toggle,
+          child: Padding(padding: padding),
+        ),
+      ),
+    )
   }
 }
