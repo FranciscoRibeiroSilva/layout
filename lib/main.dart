a9aae9738a2752a8a49f7342d55135ce69efea12
FranciscoRibeiroSilva <franciscoribeirsil@gmail.com>
2023-06-09 22:25:20 -0300

    botão expandable fan completo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(const MyApp());
@@ -26,9 +27,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 230, 230, 230),
-      body: Column(
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 230, 230, 230),
+      body: const Column(
         children: [
           Row(
             children: [
@@ -45,6 +46,44 @@ class HomePage extends StatelessWidget {
           CardRend(),
         ],
       ),
+      floatingActionButton: ExpandableFab(distance: 120, children: [
+        Column(
+          children: [
+            ActionButton(
+              icon: const Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+            ),
+            const Text('representantes')
+          ],
+        ),
+        Column(
+          children: [
+            ActionButton(
+              icon: const Icon(
+                Icons.add_shopping_cart_rounded,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+            ),
+            const Text('pedidos')
+          ],
+        ),
+        Column(
+          children: [
+            ActionButton(
+              icon: const Icon(
+                Icons.person_add,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+            ),
+            const Text('clientes'),
+          ],
+        ),
+      ]),
     );
   }
 }
@@ -52,6 +91,9 @@ class HomePage extends StatelessWidget {
 class MyCard extends StatelessWidget {
   const MyCard({super.key});
   final Color corCard = const Color.fromARGB(255, 215, 211, 226);
+  final Color cor1 = const Color.fromARGB(255, 33, 4, 87);
+  final Color cor2 = const Color.fromARGB(255, 79, 66, 111);
+  final Color cor3 = const Color.fromARGB(255, 75, 28, 162);
 
   @override
   Widget build(BuildContext context) {
@@ -90,77 +132,209 @@ class CardRend extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(color: corCard),
-        child: Row(
-          children: [
-            Icon(
-              Icons.shop_2,
-              color: corText,
-            ),
-            const Column(
-              children: [Text('RS 34.000,00'), Text('em novos pedidos')],
-            )
-          ],
-        ));
-  }
-}
-
-class ExpandableFab extends StatefulWidget {
-  const ExpandableFab({
-    super.key,
-    this.initialOpen,
-    required this.distance,
-    required this.children,
-  });
-  final bool? initialOpen;
-  final double distance;
-  final List<Widget> children;
-
-  @override
-  State<ExpandableFab> createState() => _ExpandableFabState();
-}
-
-class _ExpandableFabState extends State<ExpandableFab> {
-  bool _open = false;
-
-  @override
-  void initState(){
-    super.initState();
-    _open = widget.initialOpen ?? false;
-  }
-
-  void _toggle(){
-    setState((){
-      _open = !_open;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const SizedBox.expand(
-      child: Stack(
-        alignment: Alignment.bottomRight,
-        clipBehavior: Clip.none,
+      decoration: BoxDecoration(color: corCard),
+      child: Row(
         children: [
-          _buildTapToCloseFab();
-          _buildTapToOpenFab();
+          Icon(
+            Icons.shop_2,
+            color: corText,
+          ),
+          const Column(
+            children: [Text('RS 34.000,00'), Text('em novos pedidos')],
+          )
         ],
       ),
     );
   }
-  _buildTapToCloseFab(){
+}
+
+class ExpandableFab extends StatefulWidget {
+  const ExpandableFab(
+      {Key? key, required this.children, required this.distance})
+      : super(key: key);
+
+  final List<Widget> children;
+  final double distance;
+
+  @override
+  _ExpandableFabState createState() => _ExpandableFabState();
+}
+
+class _ExpandableFabState extends State<ExpandableFab>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _expandAnimation;
+  bool _open = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+        value: _open ? 1.0 : 0.0,
+        duration: const Duration(milliseconds: 250),
+        vsync: this);
+
+    _expandAnimation = CurvedAnimation(
+        parent: _controller,
+        curve: Curves.fastOutSlowIn,
+        reverseCurve: Curves.easeOutQuad);
+  }
+
+  void _toggle() {
+    setState(() {
+      _open = !_open;
+      if (_open) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Stack(
+        alignment: Alignment.bottomRight,
+        children: [
+          _tapToClose(),
+          ..._buildExpandableFabButton(),
+          _tapToOpen(),
+        ],
+      ),
+    );
+  }
+
+  Widget _tapToClose() {
     return SizedBox(
-      width: 56.0,
-      height: 56.0,
-      child: Material(
-        shape: const CircleBorder(),
-        clipBehavior: Clip.antiAlias,
-        elevation: 4.0,
-        child: InkWell(
-          onTap: _toggle,
-          child: Padding(padding: padding),
+      height: 55,
+      width: 55,
+      child: Center(
+        child: Material(
+          shape: const CircleBorder(),
+          clipBehavior: Clip.antiAlias,
+          elevation: 4,
+          color: const Color.fromARGB(255, 33, 4, 87),
+          child: InkWell(
+            onTap: _toggle,
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.add_sharp,
+                color: Colors.white,
+              ),
+            ),
+          ),
         ),
       ),
-    )
+    );
+  }
+
+  Widget _tapToOpen() {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 250),
+      transformAlignment: Alignment.center,
+      transform:
+          Matrix4.diagonal3Values(_open ? 0.7 : 1.0, _open ? 0.7 : 1.0, 1.0),
+      curve: Curves.easeOut,
+      child: AnimatedOpacity(
+        opacity: _open ? 0.0 : 1.0,
+        curve: Curves.easeInOut,
+        duration: const Duration(milliseconds: 250),
+        child: FloatingActionButton(
+          backgroundColor: const Color.fromARGB(255, 33, 4, 87),
+          onPressed: _toggle,
+          child: const Icon(
+            Icons.add_sharp,
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildExpandableFabButton() {
+    final List<Widget> children = <Widget>[];
+    final count = widget.children.length;
+    final step = 90.0 / (count - 1);
+
+    for (var i = 0, angleInDegrees = 0.0;
+        i < count;
+        i++, angleInDegrees += step) {
+      children.add(_ExpandableFab(
+          directionDegrees: angleInDegrees,
+          maxDistance: widget.distance,
+          progress: _expandAnimation,
+          child: widget.children[i]));
+    }
+
+    return children;
+  }
+}
+
+class _ExpandableFab extends StatelessWidget {
+  const _ExpandableFab(
+      {Key? key,
+      required this.directionDegrees,
+      required this.maxDistance,
+      required this.progress,
+      required this.child})
+      : super(key: key);
+
+  final double directionDegrees;
+  final double maxDistance;
+  final Animation<double>? progress;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: progress!,
+      builder: (context, child) {
+        final offset = Offset.fromDirection(
+            directionDegrees * (math.pi / 180), progress!.value * maxDistance);
+
+        return Positioned(
+          right: 4.0 + offset.dx,
+          bottom: 4.0 + offset.dy,
+          child: Transform.rotate(
+            angle: (1.0 - progress!.value) * math.pi / 2,
+            child: child,
+          ),
+        );
+      },
+      child: FadeTransition(
+        opacity: progress!,
+        child: child,
+      ),
+    );
+  }
+}
+
+class ActionButton extends StatelessWidget {
+  const ActionButton({Key? key, this.onPressed, required this.icon})
+      : super(key: key);
+
+  final VoidCallback? onPressed;
+  final Icon icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      shape: const CircleBorder(),
+      clipBehavior: Clip.antiAlias,
+      color: const Color.fromARGB(255, 79, 66, 111),
+      elevation: 4.0,
+      child: IconButton(
+        onPressed: onPressed,
+        icon: icon,
+      ),
+    );
   }
 }
